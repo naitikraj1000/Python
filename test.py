df0fef4f7aa86c6d008c70d5395365dba8ccd680
Naitik Raj <naitikraj10001@gmail.com>
2024-11-29 00:43:23 +0530

    New Learning Material

---
+++ test.py
@@ -0,0 +1,96 @@
+
+# def factorial (n):
+#     if n == 1:
+#         return 1
+#     return n*factorial(n-1)
+
+
+# num=int(input())
+# arr= list(map(lambda x:int(x),input().split()))
+
+# num=int(input())
+# arr= list(map(int,input().split()))
+
+
+# num=int(input())
+
+# arr=[int(x) for x in input().split()]
+# print(arr)
+
+
+# arr=[1,18,9,12,11,20,28,15,17,99]
+
+# arr=list(map(lambda x:x**2,arr))
+# print(arr)
+
+# print(list(filter( lambda x:(x%2==1),arr)))
+
+
+# arrow function in js is equal to lamda function in python
+
+# add=lambda x:x+x
+# print(add(14))
+
+
+    
+ 
+# arr=input().split()
+
+# for i in range(0,num):
+#     x=int(input())
+#     arr.append(x)
+
+# print(arr)    
+# print(factorial(num))
+
+
+
+
+# How to Iterate over 2-D array in python
+
+
+# arr_2=[[1,2,3],[4,5,6],[7,8,9],"Sachin","Rahul"]
+
+
+# for i in arr_2:
+#     if type(i)==list:
+#         for j in i:
+#             print(j)
+#     else:
+#         print(i)
+
+
+# Play with Strings
+
+# s="test python"
+
+# print("Lenght of string is ",len(s))
+# print("First character of string is ",s[0])
+
+# substring staart from ith index with length j
+# print("Substring is ",s[:])
+# print("Substring is ",s[1:5])
+
+# print("Substring is ",s[-4:-1])
+
+# s[I:J] will return substring starting from I to J-1
+# s[I:] will return substring starting from I to end of string
+# s[:J] will return substring starting from 0 to J-1
+# s[I:J:K] will return substring starting from I to J-1 with step K
+
+
+
+# Sorting in Python 
+
+arr=[1,18,9,12,11,20,28,15,17,99]
+
+arr.sort()
+print(arr)
+
+
+
+    
+        
+
+
+
